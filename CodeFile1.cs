54b2b8377f89febf3957865dd1578f34b9f5749b
Flench04 <glenn.j.sutherland@gmail.com>
2023-06-25 00:45:07 -0600

    Started Project
    The base code for the project. Must open with visual studio.

---
+++ CodeFile1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+    // The Main Window
+class program : Form
+{
+    static void Main()
+    {
+        // Starts the window
+        Application.Run(new program());
+    }
+    public program()
+    {
+        //on create
+    }
+}
